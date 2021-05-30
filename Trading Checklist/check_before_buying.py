f10c28d4bc689fbf15c4b1b2ae56ea53664669f3
Jeong Kim <jeong23291@gmail.com>
2021-05-29 22:07:25 -0700

    Add analysis on ROE. Updated check_before_buying script.

--- Trading Checklist/check_before_buying.py
+++ Trading Checklist/check_before_buying.py
@@ -2,8 +2,10 @@ import argparse
 import datetime
 
 import pandas as pd
+import numpy as np
 
 import yahoo_fin.stock_info as si
+import yfinance as yf
 
 def main():
     # define parser
@@ -19,19 +21,57 @@ def main():
         prices = list()
         for symbol in symbols:
             try:
+                full_company_name = get_company_name_from_ticker(symbol)
                 expected_price = get_expected_price(symbol)
                 live_price = get_live_price(symbol)
-                is_undervalued = expected_price > live_price
-                prices.append([expected_price, live_price, is_undervalued])
+                is_undervalued = get_is_undervalued(expected_price, live_price)
+                degree_of_undervalued = get_degree_of_undervalued(expected_price, live_price)
+                prices.append([
+                    symbol,
+                    full_company_name,
+                    expected_price,
+                    live_price,
+                    is_undervalued,
+                    degree_of_undervalued
+                ])
+                print(f'Got a price for {symbol}')
             except:
-                print("Couldn't get prices.")
+                print(f"Couldn't get a price for {symbol}.")
 
         if args.p:
-            pd.DataFrame(prices).to_csv(
-                f'prices-{symbols}-{datetime.datetime.now().time()}.csv'
-                )
+            if len(symbols) > 10:
+                ticker_symbols = symbols[:10]
+
+            export_df = pd.DataFrame(prices)
+            export_df.columns = [
+                'Symbol',
+                'Full Name',
+                'Expected Price',
+                'Live Price',
+                'Undervalued?',
+                'Degree of "Undervalued?"'
+            ]
+            export_df.to_csv(
+                f'prices-{ticker_symbols}-{datetime.datetime.now().time()}.csv',
+                index=False
+            )
+        print('It is all done!')
     else:
         print('Please provide symbols. Use -s argument.')
+
+
+def get_company_name_from_ticker(ticker) -> str:
+    '''
+    Parameters:
+        ticker (Str): Company symbol
+    
+    Returns:
+        company_name (Str): Full company name
+    '''
+    ticker_info = yf.Ticker(ticker)
+    company_name = ticker_info.info['longName']
+    
+    return company_name
 
 
 def get_expected_price(symbol) -> float:
@@ -48,6 +88,40 @@ def get_expected_price(symbol) -> float:
     per = quote_table['PE Ratio (TTM)']
 
     return eps * per
+
+
+def get_degree_of_undervalued(expected_price, live_price) -> float:
+    '''
+    Parameters:
+        expected_price (float):
+        live_price (float):
+
+    Returns:
+        (float):
+    '''
+    return np.abs(expected_price - live_price)
+
+
+def get_is_undervalued(expected_price, live_price) -> str:
+    '''
+    Parameters:
+        expected_price (float):
+        live_price (float):
+
+    Returns:
+        value (Str): Whether the live price is undervalued, overvalued, or undefined
+    '''
+    if expected_price and live_price:
+        is_undervalued = expected_price > live_price
+        if is_undervalued:
+            value = 'Undervalued'
+        else:
+            value = 'Overvalued'
+    else:
+        value = np.NaN
+    
+    return value
+    
 
 
 def get_live_price(symbol) -> float:
